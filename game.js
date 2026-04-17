e76e671865a54c24af44440728111d6a080911a1
Seb898 <seolazaro29@src.pshs.edu.ph>
2026-04-17 21:20:43 +0800

    Update game.js
    yes

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ const wordsMedium = [
     "QUILT", "TRAIL", "USING", "VOTED", "WOMAN", "ALERT", "BINGO", "ELFIN", "FIBER", "GAMER",
     "HAVEN", "BLUER", "CRONY", "ALONE", "BRINE", "CRATE", "DRAIN", "ELATE", "FLINT", "GRATE",
     "PLAIN", "SHORE", "SLATE", "SPINE", "TRIBE", "WHILE", "WRONG", "YACHT", "ZEBRA", "QUICK"
-]
+];
 const wordsHard = [
     "FJORD", "GLENT", "PRONG", "SKIRT", "ZESTY", "QUART", "XYLEM", "JUMBO", "VODKA", "NYMPH",
     "CRYPT", "LYMPH", "GLYPH", "PSALM", "WRIST", "TWIRL", "BRISK", "CLINK", "DRIFT", "FRISK",
@@ -33,7 +33,9 @@ const wordsHard = [
     "KNACK", "KNELT", "KNELT", "KNITS", "KNOBS", "KNOLL", "KNOCK", "KNOWS", "WRACK", "WRAPT",
     "WREAK", "WREST", "WRIED", "WRIES", "WRING", "WRIST", "WRITE", "WRONG", "WROTE", "YACHT",
     "YIELD", "YOKEL", "YOUNG", "YOURS", "YOUTH", "ZAPPY", "ZEBRA", "ZESTY", "ZONED", "ZONER"
-]
+];
+
+var players = [];
 
 var randomWord;
 var wordArray;
@@ -76,11 +78,6 @@ function selectDifficulty() {
         randomWord = wordsHard[Math.floor(Math.random() * wordsHard.length)];
         wordArray = randomWord.split("");
     }
-}
-
-function saveGame() {
-    localStorage.setItem("totalScore", totalScore);
-    localStorage.setItem("lives", lives);
 }
 
 document.addEventListener("keydown", function (event) {
@@ -196,11 +193,39 @@ function resetRound() {
 }
 
 function newPlayerButton() {
-    localStorage.clear();
+    addPlayer()
+    localStorage.removeItem("totalScore");
+    localStorage.removeItem("username");
+    localStorage.removeItem("lives");
+    localStorage.removeItem("difficulty");
     resetRound();
+    
 }
 
 function nextButton() {
     saveGame();
     window.location.href = "level.html"
+
 }
+
+function addPlayer() {
+    let username = localStorage.getItem("username");
+    let score = localStorage.getItem("totalScore");
+
+    let player = {
+        username: username,
+        score: score, 
+    };
+
+    let players = JSON.parse(localStorage.getItem("player"))
+
+    players.push(player);
+    localStorage.setItem("player", JSON.stringify(players));
+}
+
+
+function saveGame() {
+    localStorage.setItem("totalScore", totalScore);
+    localStorage.setItem("lives", lives);
+}
+    
