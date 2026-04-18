873655afb46ad2fd54982ea601da0e7f49c9585a
Seb898 <seolazaro29@src.pshs.edu.ph>
2026-04-18 20:48:26 +0800

    level.js

--- level.js
+++ level.js
@@ -1,6 +1,6 @@
 function saveDifficulty(level) {
     localStorage.setItem("difficulty", level);
-}
+} 
 
 var totalScore = Number(localStorage.getItem("totalScore")) || 0;
 var lives = Number(localStorage.getItem("lives")) || 3;
